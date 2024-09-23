8dbf7427ea6dbffc6dbafe173b6ac60213a4ff71
Artem_Bulychev <freemen72@yandex.ru>
2024-09-23 12:40:10 +0300

    Init commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/canban',
-  plugins: [vue()],
-});
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
